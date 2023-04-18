b9dfcd3262c4660cba2aea3ef3b972cc79aa298a
WW1027 <80759866+WW1027@users.noreply.github.com>
2023-04-18 09:08:03 +0200

    Initial commit

---
+++ app/src/main/java/ub/edu/fitdiary/MainActivity.java
@@ -0,0 +1,60 @@
+package ub.edu.fitdiary;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        ActionBar actionBar = getSupportActionBar();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.homeFragment:
+                        loadFragment(new HomeFragment(),true);
+                        break;
+                    case R.id.calendarFragment:
+                        loadFragment(new CalendarFragment(),false);
+                        break;
+                    case R.id.profileFragment:
+                        loadFragment(new ProfileFragment(),false);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        bottomNavigationView.setSelectedItemId(R.id.homeFragment);
+    }
+
+    public void loadFragment(Fragment fragment, boolean flag){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if(flag)
+            ft.add(R.id.container, fragment);
+        else
+            ft.replace(R.id.container, fragment);
+        ft.commit();
+    }
+
+}
---
+++ app/src/main/java/ub/edu/fitdiary/NewEventActivity.java
@@ -0,0 +1,13 @@
+package ub.edu.fitdiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_event);
+    }
+}

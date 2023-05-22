6ef38f94f191062db3e2cea0347aef1bec59ea53
zixin <zzx2002917@gmail.com>
2023-05-22 17:41:05 +0200

    CalendarFragment: Added an OptionsDialog for the addButton to switch between NewEvent and NewReminder.

--- app/src/main/java/ub/edu/fitdiary/view/DetailedInformationActivity.java
+++ app/src/main/java/ub/edu/fitdiary/view/DetailedInformationActivity.java
@@ -75,8 +75,7 @@ public class DetailedInformationActivity extends AppCompatActivity {
 
         /* TODO: completar la información obtenida de la base de datos */
 
-        // Inicializamos los valores de los campos
-        // Recuperar lista de sports desde BBDD
+        // Cargar los datos del evento
         newEventActivityViewModel.getEventData().observe(DetailedInformationActivity.this, new Observer<Event>() {
             @Override
             public void onChanged(Event event) {
@@ -179,6 +178,7 @@ public class DetailedInformationActivity extends AppCompatActivity {
 
     }
 
+    //Update los datos del Firebase
     protected void updateCompletion(String field, String text, String id) {
         // Como es cambio en la base de datos, se lo pedimos a viewmodel
         newEventActivityViewModel.updateCompletion(field, text, id);
